cc0d449d5b4e0804bb378c4d6d251632157d0862
Jayden Lim <jaydenlim43@gmail.com>
2025-03-27 16:54:39 +0800

    implement non-functional navbar

---
+++ tree/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home(){
+  return(
+    <>
+      <div className="container mx-auto px-4">
+        <h1>This is the Tree</h1>
+        <p>Founded by Jayden Lim</p>
+        <div className="w-full h-screen bg-green-300"></div>
+        <p>
+          Build your family tree with ease!
+        </p>
+      </div>
+    </>
+  );
+}
